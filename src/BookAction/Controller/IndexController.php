2734f00fda98195174248b13f41495a128f38a17
Paweł Niesiołowski <pavel.niesiolowski@gmail.com>
2020-08-25 21:30:13 +0200

    Improve indexing events for book
    Test various cases on controller and database level.

--- src/BookAction/Controller/IndexController.php
+++ src/BookAction/Controller/IndexController.php
@@ -3,22 +3,48 @@
 namespace App\BookAction\Controller;
 
 use App\BookAction\Domain\UseCase\ShowActionsForBookService;
+use App\Catalog\UseCase\GetBookService;
 use DateTime;
+use Exception;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class IndexController extends AbstractController
 {
     private $showActionsForBookService;
+    private $getBookService;
+    private $startDate;
+    private $logger;
 
-    public function __construct(ShowActionsForBookService $showActionsForBookService)
-    {
+    public function __construct(
+        ShowActionsForBookService $showActionsForBookService,
+        GetBookService $getBookService,
+        string $startDate,
+        LoggerInterface $logger
+    ) {
         $this->showActionsForBookService = $showActionsForBookService;
+        $this->getBookService = $getBookService;
+        $this->startDate = $startDate;
+        $this->logger = $logger;
     }
 
     public function indexActionsForBook(string $bookId)
     {
-        $afterDate = new DateTime('2019-01-01');
-        $events = $this->showActionsForBookService->showActionsForBookAfterDate($bookId, $afterDate);
-        return $this->render('book_action/index_for_book.html.twig', ['events' => $events]);
+        try {
+            $bookDTO = $this->getBookService->byId($bookId);
+            $startDate = new DateTime($this->startDate);
+        } catch (Exception $e) {
+            $this->logger->critical($e->getMessage());
+            throw new NotFoundHttpException();
+        }
+
+        $eventsViews = $this->showActionsForBookService->showActionsForBookAfterDate($bookId, $startDate);
+
+        return $this->render('book_action/index_for_book.html.twig', [
+            'title' => $bookDTO->title(),
+            'author' => $bookDTO->author(),
+            'events' => $eventsViews,
+        ]);
     }
 }
--- tests/BookAction/Controller/IndexControllerTest.php
+++ tests/BookAction/Controller/IndexControllerTest.php
@@ -3,22 +3,27 @@
 namespace App\Tests\BookAction\Controller;
 
 use App\BookAction\Domain\BookChangeEvent;
+use App\Catalog\Model\Book;
 use App\Tests\FunctionalTestCase;
 use DateTime;
 use Ramsey\Uuid\Uuid;
 
 class IndexControllerTest extends FunctionalTestCase
 {
-    public function testItIndexesActionsForBookFromTheBeginningOf2019()
+    public function testItSuccessfullyRenderPageWithEventsAfter2019()
     {
-        $bookId = Uuid::uuid1()->toString();
         $bookTitle = 'Bracia Karamazow';
+
+        $book = new Book($bookTitle);
+
+        $this->entityManager->persist($book);
+        $this->entityManager->flush();
 
         $firstEventBeforeGivenDate = new BookChangeEvent(
             BookChangeEvent::RECEIVE,
             5,
             new DateTime('2019-01-01'),
-            $bookId,
+            $book->getId(),
             $bookTitle
         );
 
@@ -26,8 +31,45 @@ class IndexControllerTest extends FunctionalTestCase
         $this->entityManager->flush();
 
         $client = static::createClient();
-        $client->xmlHttpRequest('GET', '/bookaction/' . $bookId);
+        $client->xmlHttpRequest('GET', '/bookaction/' . $book->getId());
         $response = $client->getResponse();
+
         self::assertTrue($response->isOk());
+    }
+
+    public function testItSuccessfullyRenderPageWithoutEventsAfter2019()
+    {
+        $bookTitle = 'Bracia Karamazow';
+
+        $book = new Book($bookTitle);
+
+        $this->entityManager->persist($book);
+        $this->entityManager->flush();
+
+        $firstEventBeforeGivenDate = new BookChangeEvent(
+            BookChangeEvent::RECEIVE,
+            5,
+            new DateTime('2018-01-01'),
+            $book->getId(),
+            $bookTitle
+        );
+
+        $this->entityManager->persist($firstEventBeforeGivenDate);
+        $this->entityManager->flush();
+
+        $client = static::createClient();
+        $client->xmlHttpRequest('GET', '/bookaction/' . $book->getId());
+        $response = $client->getResponse();
+
+        self::assertTrue($response->isOk());
+    }
+
+    public function testItReturnsNotFoundIfBookDoesNotExist()
+    {
+        $client = static::createClient();
+        $client->xmlHttpRequest('GET', '/bookaction/' . Uuid::uuid1()->toString());
+        $response = $client->getResponse();
+
+        self::assertSame(404, $response->getStatusCode());
     }
 }
